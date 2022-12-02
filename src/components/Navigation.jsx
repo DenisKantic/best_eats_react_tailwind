78445500b7f060982bf7b99ef969c61abfbed960
DenisKantic <denis.kantic18@gmail.com>
2022-12-02 23:33:20 +0100

    created main part with image and text, needs a little more tweaks but it's responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,12 +13,12 @@ const Navigation = () => {
 
 
   return (
-    <div className='max-w-[95%] mx-auto flex justify-between items-center p-3'>
+    <div className='max-w-[90%] mx-auto flex justify-between items-center p-4'>
 
         {/* left side */}
         <div className='flex items-center'>
 
-        <AiOutlineMenu className='mx-2 cursor-pointer' size={20} onClick={()=> setNav(!nav)}></AiOutlineMenu>
+        <AiOutlineMenu className='mr-2 cursor-pointer' size={25} onClick={()=> setNav(!nav)}></AiOutlineMenu>
         
         <h1 className='sm:text-3xl lg:text-xl flex'>Best <span className='font-bold'>Eats</span></h1>
         
