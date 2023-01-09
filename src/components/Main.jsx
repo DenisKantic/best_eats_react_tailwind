ddf670e5007d63a09937d492f67ced41582917e3
DenisKantic <denis.kantic18@gmail.com>
2023-01-09 22:38:31 +0100

    threeItems.jsx mobile responsive is done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,8 @@ const Main = () => {
         <div className="max-w-[90%] mx-auto p-4" >
             <div className="max-h-[500px] relative">
             <div className="w-full h-full bg-black/40 absolute  flex flex-col justify-center">
-                <h1 className="px-6 text-white font-bold  xxs:text-3xl medium:text-5xl largerStart:text-6xl lg:text-7xl">The <span className="text-orange-500">Best</span></h1>
-                <h1 className="px-6 text-white font-bold  xxs:text-3xl medium:text-5xl largerStart:text-6xl lg:text-7xl"><span className="text-orange-500">Food</span> Delivered</h1>
+                <h1 className="px-6 text-white font-bold  xss:text-3xl medium:text-5xl largerStart:text-6xl lg:text-7xl">The <span className="text-orange-500">Best</span></h1>
+                <h1 className="px-6 text-white font-bold  xss:text-3xl medium:text-5xl largerStart:text-6xl lg:text-7xl"><span className="text-orange-500">Food</span> Delivered</h1>
             </div>
 
             <img className="w-full max-h-[500px] object-cover" src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg" alt="" />
