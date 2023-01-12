685b9469a52ef60aae2cd705fd1e21a6269640cb
DenisKantic <denis.kantic18@gmail.com>
2023-01-12 11:51:57 +0100

    changed the three pictures

--- src/components/ThreeItem.jsx
+++ src/components/ThreeItem.jsx
@@ -19,14 +19,14 @@ const ThreeItem = () => {
        </div>
 
 
-       <div className="rounded-xl w-[500px] h-full relative overflow-hidden xss:w-full mb-4 mr-4">
+       <div className="rounded-xl w-[500px] hhttps://images.pexels.com/photos/1049626/pexels-photo-1049626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1-full relative overflow-hidden xss:w-full mb-4 mr-4">
        <div className="w-full h-full bg-black/40 absolute  flex flex-col justify-center">
        <h1 className="absolute top-2 p-2 text-2xl text-white font-bold xss:text-lg">Sun's Out, BOGO's Out</h1>
         <h3 className="absolute top-8 p-2 text-lg text-white xss:text-sm">Through 8/26</h3>
 
         <button className="absolute bottom-4 ml-2 bg-white py-2 px-5 rounded-xl xss:px-3 text-sm">Order Now</button>
         </div>
-        <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+        <img src="https://images.pexels.com/photos/1049626/pexels-photo-1049626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
         className="w-full  max-h-[170px] object-cover rounded-xl" alt="" />
        </div>
 
@@ -38,7 +38,7 @@ const ThreeItem = () => {
 
         <button className="absolute bottom-4 ml-2 bg-white py-2 px-5 rounded-xl xss:px-3 text-sm">Order Now</button>
         </div>
-        <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+        <img src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
         className="w-full  max-h-[170px] object-cover rounded-xl" alt="" />
        </div>
 
